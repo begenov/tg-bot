46ea828afb69a9b3ca662fabdb7376fb60ae2ce8
begenov <begenovorazali@icloud.com>
2023-05-10 21:19:17 +0600

    app in run function

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,13 @@
 package app
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func Run() error {
+	// config
+	// tgAPI
+	// w
+	bot, err := tgbotapi.NewBotAPI()
+	return nil
+}
