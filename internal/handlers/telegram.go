48484e4106e757de03c8b5318e3e867b016519ff
Alikhan <student@ALEM-F3-R01-02.alem.school>
2023-05-14 16:59:33 +0600

    stage 4,5

--- internal/handlers/telegram.go
+++ internal/handlers/telegram.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/begenov/tg-bot/internal/services"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -12,11 +12,13 @@ type TelegramAPI struct {
 	services *services.Service
 	usermapa map[int64]*User
 }
+
 type User struct {
 	Stage int
 	lang  string
 	name  string
 	phone string
+	aim   string
 }
 
 func NewTelegramAPI(bot *tgbotapi.BotAPI, servces *services.Service) *TelegramAPI {
@@ -47,6 +49,8 @@ func (api *TelegramAPI) StartTelegramAPI() error {
 		case 0:
 			if update.CallbackQuery != nil {
 				lang := update.CallbackQuery.Data
+				api.usermapa[chatId].lang = lang
+				// fmt.Println(lang, "-------------------------")
 				api.usermapa[chatId].Stage = 1
 
 				msg2 := tgbotapi.NewMessage(chatId, "")
@@ -72,7 +76,7 @@ func (api *TelegramAPI) StartTelegramAPI() error {
 				msg2 := tgbotapi.NewMessage(chatId, "")
 
 				if api.usermapa[chatId].lang == "kazakh" {
-					msg.Text = "Сәлем" + name
+					msg.Text = "Сәлем, " + name
 
 					shareButton.Text = "Нөмірмен бөлісу"
 
@@ -100,7 +104,19 @@ func (api *TelegramAPI) StartTelegramAPI() error {
 		case 2:
 			if update.Message.Contact != nil {
 				phoneNumber := update.Message.Contact.PhoneNumber
-				log.Fatal(phoneNumber)
+				// log.Fatal(phoneNumber)
+				api.usermapa[chatId].phone = phoneNumber
+				api.usermapa[chatId].Stage = 4
+				if api.usermapa[chatId].lang == "kazakh" {
+					msg.Text = "Сізге коды бар SMS хабарлама келеді. Оны енгізіңіз."
+				} else {
+					msg.Text = "Вы получите SMS-уведомление с кодом. Введите его, пожалуйста."
+				}
+				api.bot.Send(msg)
+				continue
+			}
+			if update.Message != nil {
+				phoneNumber := update.Message.Text
 				api.usermapa[chatId].phone = phoneNumber
 				api.usermapa[chatId].Stage = 4
 				if api.usermapa[chatId].lang == "kazakh" {
@@ -114,17 +130,57 @@ func (api *TelegramAPI) StartTelegramAPI() error {
 		case 4:
 			if update.Message != nil {
 				code := update.Message.Text
-				if code != "0000" {
+				if code == "0000" {
 					if api.usermapa[chatId].lang == "kazakh" {
 						msg.Text = "Сізге коды бар SMS хабарлама келеді. Оны енгізіңіз."
 					} else {
 						msg.Text = "Вы получите SMS-уведомление с кодом. Введите его, пожалуйста."
 					}
-					continue
+					// api.usermapa[chatId].Stage = 5
+					// // api.bot.Send(msg)
+					// continue
 				}
-				api.usermapa[chatId].Stage = 4
+				msg.Text = "Ваш код получен, спасибо!"
+				api.bot.Send(msg)
+				msg1 := tgbotapi.NewMessage(chatId, "")
+				msg1.Text = fmt.Sprintf("%s, спасибо за регистрацию! Вы ищите работу или сотрудника?", api.usermapa[chatId].name)
+				inlineKeyboard := tgbotapi.NewInlineKeyboardMarkup(
+					tgbotapi.NewInlineKeyboardRow(
+						tgbotapi.NewInlineKeyboardButtonData("ищу работу", "work"),
+						tgbotapi.NewInlineKeyboardButtonData("ищу сотрудника", "employee"),
+					),
+				)
+				msg1.ReplyMarkup = inlineKeyboard
+				api.usermapa[chatId].Stage = 5
+				api.bot.Send(msg1)
+				continue
 
 			}
+			// fallthrough
+		case 5:
+			if update.CallbackQuery != nil {
+				api.usermapa[chatId].aim = update.CallbackQuery.Data
+				api.usermapa[chatId].Stage = 6
+				fmt.Println("--------------The aim is ", api.usermapa[chatId].aim)
+				if api.usermapa[chatId].lang == "kazakh" {
+					msg.Text = "Сопроводительное письмо"
+				} else {
+					msg.Text = "Сопроводительное письмо"
+				}
+				api.bot.Send(msg)
+				continue
+			}
+			// if update.Message != nil {
+			// 	api.usermapa[chatId].aim = update.Message.Text
+			// 	api.usermapa[chatId].Stage = 6
+			// 	if api.usermapa[chatId].lang == "kazakh" {
+			// 		msg.Text = "Сопроводительное письмо"
+			// 	} else {
+			// 		msg.Text = "Сопроводительное письмо"
+			// 	}
+			// 	api.bot.Send(msg)
+			// 	continue
+			// }
 
 		}
 	}
