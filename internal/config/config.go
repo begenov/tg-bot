3af8aade836540ab5ed6e17d11b2e9d4e4324b85
begenov <begenovorazali@icloud.com>
2023-05-14 14:07:10 +0600

    new branch

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/subosito/gotenv"
+)
+
+type Config struct {
+	TelegramAPI struct {
+		Token   string
+		BaseURL string
+	}
+	DB struct {
+		Driver string
+		DSN    string
+	}
+}
+
+const path = "./.env"
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+
+	err := gotenv.Load(path)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.TelegramAPI.Token = os.Getenv("TELEGRAM_API_TOKEN")
+	cfg.TelegramAPI.BaseURL = os.Getenv("TELEGRAM_API_BASE_URL")
+
+	cfg.DB.Driver = os.Getenv("DRIVER")
+	cfg.DB.DSN = os.Getenv("DSN")
+	log.Println(cfg)
+	return cfg, nil
+}
---
+++ internal/handlers/hello.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (api *TelegramAPI) Hello(message *tgbotapi.Message, chatId int64) {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Здравствуйте, это Telegram-bot по поиску работы и сотрудников.\nВыберите язык:")
+	inlineKeyboard := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("казахский", "kazakh"),
+			tgbotapi.NewInlineKeyboardButtonData("русский", "russian"),
+		),
+	)
+	msg.ReplyMarkup = inlineKeyboard
+
+	api.usermapa[chatId] = &User{Stage: 0}
+
+	api.bot.Send(msg)
+}
