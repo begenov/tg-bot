3878ba4ddf3728fb2fff1a4d898146d29a7b3abc
ali <student@ALEM-F3-R01-02.alem.school>
2023-05-14 18:10:24 +0600

    next

--- internal/handlers/telegram.go
+++ internal/handlers/telegram.go
@@ -34,8 +34,8 @@ func (api *TelegramAPI) StartTelegramAPI() error {
 
 	u.Timeout = 60
 	updates := api.bot.GetUpdatesChan(u)
-	for update := range updates {
 
+	for update := range updates {
 		chatId := update.FromChat().ID
 		msg := tgbotapi.NewMessage(chatId, "")
 
